f26794bf2ac2e3304bf92b2d4c75e3a11f7f81c8
Lennoc <alejandrogarciasoria7@gmail.com>
2024-06-10 19:14:14 +0200

    este es el primer ejercicio pero sin acabar

---
+++ Staff/Lennoc/ejercicico-condiciones.js
@@ -0,0 +1,24 @@
+
+const edad = 28
+console.log("muestra la edad")
+
+// HAZ UNA COMPROBACIÓN, QUE PINTE POR CONSOLA CUANDO EDAD SEA 28
+
+if (edad === 28) {
+    console.log("esta es la edad correcta")
+}
+
+// HAZ UNA COMPROBACIÓN, QUE PINTE POR CONSOLA, CUANDO EDAD SEA DIFERENTE A 30
+
+const adrian = 'adrian'
+const lennoc = 'lennoc'
+
+if (edad !== 30){
+    console.log("no es 30")
+}
+// HAZ UNA COMPROBACIÓN, QUE PINTE POR CONSOLA CUANDO LOS DOS NOMBRES COINCIDAN
+
+
+// HAZ OTRA COMPROBACIÓN, QUE PINTE CUANDO SOLO UNO DE LOS NOMBRES COINCIDAN
+
+// HAZ OTRA COMPROBACIÓN, CUANDO NINGUN NOMBRE COINCIDA

32525348fad83f091a507295c391e487dc28df8a
Lennoc <alejandrogarciasoria7@gmail.com>
2024-06-10 19:48:41 +0200

    este es el ejercicio resuelto

--- Staff/Lennoc/ejercicico-condiciones.js
+++ Staff/Lennoc/ejercicico-condiciones.js
@@ -9,16 +9,22 @@ if (edad === 28) {
 }
 
 // HAZ UNA COMPROBACIÓN, QUE PINTE POR CONSOLA, CUANDO EDAD SEA DIFERENTE A 30
-
-const adrian = 'adrian'
-const lennoc = 'lennoc'
-
 if (edad !== 30){
     console.log("no es 30")
 }
+
 // HAZ UNA COMPROBACIÓN, QUE PINTE POR CONSOLA CUANDO LOS DOS NOMBRES COINCIDAN
+const adrian = 'adrian'
+const lennoc = 'valoruno'
 
-
+if (adrian === "adrian" && lennoc === "valoruno"){
+    console.log("muestra los nombres porque al comprobar las dos condiciones se cumplen")
+}
 // HAZ OTRA COMPROBACIÓN, QUE PINTE CUANDO SOLO UNO DE LOS NOMBRES COINCIDAN
-
+if (adrian ==="adrian" || lennoc ==="lennoc"){
+    console.log("ahora muestra cuando solo se cumple uno de ellos")
+}
 // HAZ OTRA COMPROBACIÓN, CUANDO NINGUN NOMBRE COINCIDA
+if (adrian !== "dd" && lennoc !== "dad"){
+    console.log("ahora muestra cuando no coincide")
+}

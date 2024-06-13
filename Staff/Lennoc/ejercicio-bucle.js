75e148ef30674d9dc255000fe233b77298da226d
Lennoc <alejandrogarciasoria7@gmail.com>
2024-06-13 18:00:08 +0200

    ejercicio resuelto bucles

---
+++ Staff/Lennoc/ejercicio-bucle.js
@@ -0,0 +1,22 @@
+// BUCLE WHILE HASTA QUE EL CONTADOR SEA MENOR O IGUAL A 100
+let contador = 15
+while (contador < 101) {
+    console.log('valor del contador menos o igual: ', contador)
+    contador = contador +1
+}
+// BUCLE WHILE HASTA QUE EL CONTADOR 2 SEA IGUAL A 0
+let contador2 = 50
+while(contador2 > -1){
+    console.log('valor del contador restando hasta: ', contador2)
+    contador2 = contador2 -1
+}
+
+// BUCLE FOR INCREMENTANDO LA VARIABLE HASTA QUE LA VARIABLE DE CONTROL SEA IGUAL A 15
+for(variableControl = 0; variableControl <= 15; variableControl ++) {
+    console.log('valor de control: ',variableControl)
+}
+
+// BUCLE FOR DISMINUYENDO VALOR HASTA QUE SEA VALOR IGUAL A 0
+for(variableControl = 10; variableControl >= 0; variableControl --) {
+    console.log('valor de control 2: ',variableControl)
+}
